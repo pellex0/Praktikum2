0606f0b89cf3a0a73678634566c18eaba5b6d594
pellex0 <Pellex0@gmail.com>
2025-11-19 15:28:48 +0100

    implemented main

---
+++ PythagoraeischeZahltripelMain.java
@@ -0,0 +1,8 @@
+public static void main (
+    String[] args
+) {
+    PythagoraeischeZahlentripel array1 = new PythagoraeischeZahlentripel(1,1000); 
+    array1.gibAnzahlAus();
+    PythagoraeischeZahlentripel array2 = new PythagoraeischeZahlentripel(1001,2000); 
+    array2.gibAnzahlAus();
+}
